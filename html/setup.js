e9da9506e02603f132e7d92db8592ce1754107f0
HS <notear0@gmail.com>
2018-04-20 00:03:44 +0900

    drawCard 및 initGame 모듈 작성 중

--- html/setup.js
+++ html/setup.js
@@ -16,6 +16,10 @@ function Player(name, money){
     this.name = name;
     this.winCount = 0;
     this.loseCount = 0;
+
+    this.cards = new Card[11];
+    this.cardSum = 0;
+    this.cardCount = 0;
 }
 /*
     아래는 Player 객체의 메소드 선언하는 것과 같음.
@@ -26,6 +30,13 @@ function Player(name, money){
  */
 Player.prototype.getName = function() {
     return this.name;
+}
+
+var suits = [0, 1, 2, 3]; // 0 spade 1 heart 2 diamond 3 club
+
+function Card(suit, num) {
+    this.suit = suit;
+    this.num = num;
 }
 
 ///////////////////////////////
@@ -42,8 +53,64 @@ var GameManager = {
 
 ///////////////////////////////
 
+var deck = new int[4][13];
+var end;
+var playersCnt = 2;
+var players = Player[2];
+var isGameEnded;
+
+function initGame() {
+    document.getElementById("gameTitle").value = GameManager.title;
+
+    end = 0;
+    isGameEnded = false;
+    var initMoney = 100000;
+
+    players[0] = new Player("dealer",initMoney * 50);
+    players[1] = new Player("player1",initMoney);
+
+    gameStart();
+}
+
+function gameStart() {
+    var winner;
+    while (isGameEnded) {
+        drawCards();
+    }
+}
+
+function drawCards() {
+    // 카드 드로우
+    for (var i = 0; i < playersCnt; ++i) {
+        var suit, number;
+        while (true) {
+            suit = Math.floor(Math.random() * 4);
+            number = Math.floor(Math.random() * 13);
+
+            if (deck[suit][number] == 0) {
+                deck[suit][number] = 1;
+                break;
+            }
+        }
+        players[i].cards[players[i].cardCount++] = new Card(suit, number);
+        players[i].cardSum += number;
+    }
+}
+
+function cardOpen() {
+
+}
+
+function endGame() {
+    isGameEnded = true;
+}
+
+///////////////////////////////
+
 // onload 함수는 html element가 load된 후에 
 // 불리는 함수. entry point라고 생각하면 될 듯
-window.onload = function() {
+window.onload = function() {   
 
+    document.getElementById("winCount").value = GameManager.title;
+    document.getElementById("money").value = GameManager.title;
 }
